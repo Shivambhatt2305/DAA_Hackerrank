5559e29a270d2197c4d26c91657ef2d34e1b45de
Shivam Bhatt <143023404+Shivambhatt2305@users.noreply.github.com>
2025-08-01 08:10:20 +0530

    Update Day_of_programmer.py

--- Day_of_programmer.py
+++ Day_of_programmer.py
@@ -15,7 +15,10 @@ import sys
 
 def dayOfProgrammer(year):
     # Write your code here
-    if year%4==0:
+    if year == 1918:
+        return f"26.09.{year}"
+    elif (year < 1918 and year % 4 == 0) or\
+         (year > 1918 and year%400==0 or (year % 4 == 0 and year % 100 != 0)):
         return f"12.09.{year}"
     else:
         return f"13.09.{year}"
